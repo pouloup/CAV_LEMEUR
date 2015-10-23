8c900089c22cdbcc3d2b4c714c9796250dd6d6c5
pouloup <paul.ledenn@wanadoo.fr>
2015-10-23 18:21:57 +0200

    Initial commit

---
+++ build/CMakeFiles/SR_LEMEUR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SR_LEMEUR.dir/src/main.cpp.o"
+  "SR_LEMEUR.pdb"
+  "SR_LEMEUR"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/SR_LEMEUR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "opencv2/core.hpp"
+
+int main(int argc, char** argv){
+
+  cv::Mat a;
+
+  return 0;
+}
